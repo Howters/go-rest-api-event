b3712d458a3a5cb944c7065e4ba408005422ed20
Yougit config --global user.email <hans.khomulia@binus.edu>
2024-09-02 20:30:57 +0700

    feat: added delete event api

--- routes/events.go
+++ routes/events.go
@@ -71,3 +71,83 @@ func createEvent(context *gin.Context) {
 		"event": event,
 	})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err:=strconv.ParseInt(context.Param("id"),10 , 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message" : "Could not parse event id.",
+		})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message" : "Could not find event",
+		})
+		return
+	}
+
+	var updateEvent models.Event
+	err = context.ShouldBindJSON(&updateEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message" : "Could not parse request data",
+		})
+		return
+	}
+
+	updateEvent.ID = eventId
+	err = updateEvent.Update()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message" : "Could not update event",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"message" : "Event updated succesfull",
+	})
+}
+
+func deleteEvent(context *gin.Context) {
+	eventId, err:=strconv.ParseInt(context.Param("id"),10 , 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message" : "Could not parse event id.",
+		})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message" : "Could not find event",
+		})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message" : "Could not delete event",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"message" : "Event deleted succesfull",
+	})
+
+
+
+}
